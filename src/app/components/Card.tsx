c17e73eadc55a3bdd83625fb49e93731d0569d8d
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-27 20:57:30 +0700

    🆙(card.tsx): create interface

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 
-const Card = () => {
+interface ICardProps {
+  color: string;
+  image: string;
+  title: string;
+  description: string;
+  onClick?: () => void;
+}
+
+const Card: React.FC<ICardProps> = (props) => {
   return (
-    <div className="flex flex-col p-7 bg-[#e38826]">
+    <div className={`flex flex-col p-7 bg-${props.color}`}>
       <div className="flex flex-col gap-4">
         <div className="">
-          <img src="/icon-sedans.svg"></img>
+          <img src={props.image}></img>
         </div>
-        <div className="font-bold text-5xl">Title</div>
-        <div className="font-normal text-[15px]">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
-          illum obcaecati natus fugit perferendis doloremque dolorum
-          dignissimos, debitis exercitationem inventore autem alias saepe
-          corporis repudiandae. Beatae eligendi quibusdam molestiae voluptas.
-        </div>
+        <div className="font-bold text-5xl">{props.title}</div>
+        <div className="font-normal text-[15px]">{props.description}</div>
       </div>
       <div className="mt-4">
-        <button className="bg-white py-3 px-8 text-[#e38826] border-collapse rounded-full">
+        <a
+          href="#"
+          className={`bg-white py-3 px-8 text-${props.color} border-collapse rounded-full`}
+        >
           Learn More
-        </button>
+        </a>
       </div>
     </div>
   );
