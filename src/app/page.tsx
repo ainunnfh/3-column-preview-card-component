4ceb44673b4d9a45524899aa281b604a804748ec
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-28 11:10:40 +0700

    🆙(page.tsx): change color props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,21 @@ import Card from "./components/Card";
 
 export default function Home() {
   return (
-    <div className="flex flex-col md:flex-row gap-4 md:h-screen">
+    <div className="flex flex-col md:flex-row h-screen">
       <Card
-        color="[#e38826]"
+        color="orange"
         image="/icon-sedans.svg"
         title="Sedans"
         description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
       />
       <Card
-        color="[#006970]"
+        color="teal"
         image="/icon-suvs.svg"
         title="Suvs"
         description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
       />
       <Card
-        color="[#004241]"
+        color="dark-green"
         image="/icon-luxury.svg"
         title="Luxury"
         description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
