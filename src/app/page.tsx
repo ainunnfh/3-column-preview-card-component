4aef6013b8855a082e45f715a6083687e328c4b2
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-05 22:50:32 +0700

    🆙(page.tsx): change text desc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
         color="teal"
         image="/icon-suvs.svg"
         title="Suvs"
-        description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+        description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
       />
       <Card
         color="dark-green"
         image="/icon-luxury.svg"
         title="Luxury"
-        description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+        description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
       />
     </div>
   );
