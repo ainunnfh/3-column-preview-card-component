5e544e997092b6340fa2641317b748be11ccd8cf
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-05 23:04:11 +0700

    💄update (card.tsx): change height for desktop size

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -22,23 +22,23 @@ const colorText = {
 const Card: React.FC<ICardProps> = (props) => {
   console.log(`Card ${props.color}`);
   return (
-    <div className={`flex flex-col p-7 ${colorClasses[props.color]} h-screen`}>
-      <div className={`flex flex-col gap-4`}>
+    <div className={`flex flex-col p-7 ${colorClasses[props.color]} h-screen `}>
+      <div className={`flex flex-col gap-4 justify-evenly md:gap-0 md:h-3/4`}>
         <div className="">
           <img src={props.image}></img>
         </div>
         <div className="font-bold text-5xl">{props.title}</div>
         <div className="font-normal text-[15px]">{props.description}</div>
-      </div>
-      <div className="mt-4">
-        <a
-          href="#"
-          className={`bg-white py-3 px-8 border-collapse rounded-full text-${
-            colorText[props.color]
-          }`}
-        >
-          Learn More
-        </a>
+        <div className="">
+          <a
+            href="#"
+            className={`bg-white py-3 px-8 border-collapse rounded-full text-${
+              colorText[props.color]
+            }`}
+          >
+            Learn More
+          </a>
+        </div>
       </div>
     </div>
   );
