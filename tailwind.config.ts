9e7af96f0d9143f43ba6a1b78fb37b5aa331ef19
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-06 21:25:15 +0700

    🆙(tailwind.config): config new font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        lexend: ["Lexend_Deca", "sans-serif"],
+      },
     },
   },
   plugins: [],
